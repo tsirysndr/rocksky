a617d5609b89779fb43ddc1f1e3fae9318051c77
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-02-06 10:26:19 +0300

    fix zod validation, make optional also nullable

--- rockskyapi/rocksky-auth/src/types/track.ts
+++ rockskyapi/rocksky-auth/src/types/track.ts
@@ -6,20 +6,20 @@ export const trackSchema = z.object({
   album: z.string().nonempty(),
   albumArtist: z.string().nonempty(),
   duration: z.number(),
-  mbId: z.string().optional(),
-  albumArt: z.string().optional(),
-  trackNumber: z.number().optional(),
+  mbId: z.string().optional().nullable(),
+  albumArt: z.string().optional().nullable(),
+  trackNumber: z.number().optional().nullable(),
   releaseDate: z
     .string()
     .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid date format. Use YYYY-MM-DD.")
     .transform((v) => new Date(v))
-    .optional(),
+    .optional().nullable(),
 
-  year: z.number().optional(),
+  year: z.number().optional().nullable(),
   discNumber: z.number().optional().nullable(),
-  lyrics: z.string().optional(),
-  composer: z.string().optional(),
-  copyrightMessage: z.string().optional(),
+  lyrics: z.string().optional().nullable(),
+  composer: z.string().optional().nullable(),
+  copyrightMessage: z.string().optional().nullable(),
 });
 
 export type Track = z.infer<typeof trackSchema>;
