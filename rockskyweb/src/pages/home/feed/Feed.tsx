58c1f69c746f84577ff82d36303cc5db0573cf08
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-02-09 09:46:04 +0300

    add react content loader

--- rockskyweb/src/pages/home/feed/Feed.tsx
+++ rockskyweb/src/pages/home/feed/Feed.tsx
@@ -5,6 +5,7 @@ import { StatefulTooltip } from "baseui/tooltip";
 import { HeadingMedium, LabelMedium } from "baseui/typography";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import ContentLoader from "react-content-loader";
 import { Link } from "react-router";
 import SongCover from "../../../components/SongCover";
 import useFeed from "../../../hooks/useFeed";
@@ -33,39 +34,69 @@ function Feed() {
         Recently played
       </HeadingMedium>
 
-      <div style={{ paddingBottom: 100 }}>
-        <FlexGrid
-          flexGridColumnCount={[1, 2, 3]}
-          flexGridColumnGap="scale800"
-          flexGridRowGap="scale800"
+      {data.length === 0 && (
+        <ContentLoader
+          width={800}
+          height={575}
+          viewBox="0 0 800 575"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
         >
-          {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            data.map((song: any) => (
-              <FlexGridItem {...itemProps} key={song.id}>
-                <Link to={`/${song.uri.split("at://")[1]}`}>
-                  <SongCover
-                    cover={song.cover}
-                    artist={song.artist}
-                    title={song.title}
-                  />
-                </Link>
-                <LabelMedium color={"#ff2876"}>@{song.user}</LabelMedium>
-                <LabelMedium>is listening to this song</LabelMedium>
-                <StatefulTooltip
-                  content={dayjs(song.date).format("MMMM D, YYYY [at] HH:mm A")}
-                  returnFocus
-                  autoFocus
-                >
-                  <LabelMedium color="#42576ca6">
-                    {dayjs(song.date).fromNow()}
-                  </LabelMedium>
-                </StatefulTooltip>
-              </FlexGridItem>
-            ))
-          }
-        </FlexGrid>
-      </div>
+          <rect x="12" y="9" rx="2" ry="2" width="140" height="10" />
+          <rect x="14" y="30" rx="2" ry="2" width="667" height="11" />
+          <rect x="12" y="58" rx="2" ry="2" width="211" height="211" />
+          <rect x="240" y="57" rx="2" ry="2" width="211" height="211" />
+          <rect x="467" y="56" rx="2" ry="2" width="211" height="211" />
+          <rect x="12" y="283" rx="2" ry="2" width="211" height="211" />
+          <rect x="240" y="281" rx="2" ry="2" width="211" height="211" />
+          <rect x="468" y="279" rx="2" ry="2" width="211" height="211" />
+          <circle cx="286" cy="536" r="12" />
+          <circle cx="319" cy="535" r="12" />
+          <circle cx="353" cy="535" r="12" />
+          <rect x="378" y="524" rx="0" ry="0" width="52" height="24" />
+          <rect x="210" y="523" rx="0" ry="0" width="52" height="24" />
+          <circle cx="210" cy="535" r="12" />
+          <circle cx="428" cy="536" r="12" />
+        </ContentLoader>
+      )}
+
+      {data.length > 0 && (
+        <div style={{ paddingBottom: 100 }}>
+          <FlexGrid
+            flexGridColumnCount={[1, 2, 3]}
+            flexGridColumnGap="scale800"
+            flexGridRowGap="scale800"
+          >
+            {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              data.map((song: any) => (
+                <FlexGridItem {...itemProps} key={song.id}>
+                  <Link to={`/${song.uri.split("at://")[1]}`}>
+                    <SongCover
+                      cover={song.cover}
+                      artist={song.artist}
+                      title={song.title}
+                    />
+                  </Link>
+                  <LabelMedium color={"#ff2876"}>@{song.user}</LabelMedium>
+                  <LabelMedium>is listening to this song</LabelMedium>
+                  <StatefulTooltip
+                    content={dayjs(song.date).format(
+                      "MMMM D, YYYY [at] HH:mm A"
+                    )}
+                    returnFocus
+                    autoFocus
+                  >
+                    <LabelMedium color="#42576ca6">
+                      {dayjs(song.date).fromNow()}
+                    </LabelMedium>
+                  </StatefulTooltip>
+                </FlexGridItem>
+              ))
+            }
+          </FlexGrid>
+        </div>
+      )}
     </Container>
   );
 }
--- rockskyweb/src/pages/song/Song.tsx
+++ rockskyweb/src/pages/song/Song.tsx
@@ -12,6 +12,7 @@ import {
 import { useAtomValue } from "jotai";
 import numeral from "numeral";
 import { useEffect, useState } from "react";
+import ContentLoader from "react-content-loader";
 import { useParams } from "react-router";
 import { profileAtom } from "../../atoms/profile";
 import SongCover from "../../components/SongCover";
@@ -40,63 +41,75 @@ const Song = () => {
 
   return (
     <Main>
-      {song && (
-        <div style={{ paddingBottom: 100, paddingTop: 50 }}>
-          <Group>
-            <SongCover cover={song?.cover} size={150} />
-            <div style={{ marginLeft: 20 }}>
-              <HeadingMedium margin={0}>{song?.title}</HeadingMedium>
-              <LabelLarge margin={0}>{song?.artist}</LabelLarge>
-              <LabelSmall marginTop={"15px"}>Listeners</LabelSmall>
-              <HeadingXSmall margin={0}>
-                {numeral(song?.listeners).format("0,0")}
-              </HeadingXSmall>
-            </div>
-          </Group>
-          {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            song?.tags.map((tag: any) => (
-              <Tag closeable={false} kind={KIND.purple}>
-                {tag}
-              </Tag>
-            ))
-          }
+      <div style={{ paddingBottom: 100, paddingTop: 50 }}>
+        {!song && (
+          <ContentLoader viewBox="0 0 520 160" height={160} width={400}>
+            <rect x="220" y="21" rx="10" ry="10" width="294" height="20" />
+            <rect x="221" y="61" rx="10" ry="10" width="185" height="20" />
+            <rect x="304" y="-46" rx="3" ry="3" width="350" height="6" />
+            <rect x="371" y="-45" rx="3" ry="3" width="380" height="6" />
+            <rect x="484" y="-45" rx="3" ry="3" width="201" height="6" />
+            <rect x="48" y="21" rx="8" ry="8" width="150" height="150" />
+          </ContentLoader>
+        )}
+        {song && (
+          <>
+            <Group>
+              <SongCover cover={song?.cover} size={150} />
+              <div style={{ marginLeft: 20 }}>
+                <HeadingMedium margin={0}>{song?.title}</HeadingMedium>
+                <LabelLarge margin={0}>{song?.artist}</LabelLarge>
+                <LabelSmall marginTop={"15px"}>Listeners</LabelSmall>
+                <HeadingXSmall margin={0}>
+                  {numeral(song?.listeners).format("0,0")}
+                </HeadingXSmall>
+              </div>
+            </Group>
+            {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              song?.tags.map((tag: any) => (
+                <Tag closeable={false} kind={KIND.purple}>
+                  {tag}
+                </Tag>
+              ))
+            }
 
-          <div style={{ marginTop: 150 }}>
-            <LabelMedium marginBottom={"10px"}>Shoutbox</LabelMedium>
-            {profile && (
-              <>
-                <Textarea
-                  placeholder={`@${profile?.handle}, share your thoughts about this song`}
-                  resize="vertical"
-                  overrides={{
-                    Input: {
-                      style: {
-                        width: "770px",
+            <div style={{ marginTop: 150 }}>
+              <LabelMedium marginBottom={"10px"}>Shoutbox</LabelMedium>
+              {profile && (
+                <>
+                  <Textarea
+                    placeholder={`@${profile?.handle}, share your thoughts about this song`}
+                    resize="vertical"
+                    overrides={{
+                      Input: {
+                        style: {
+                          width: "770px",
+                        },
                       },
-                    },
-                  }}
-                  maxLength={1000}
-                />
-                <div
-                  style={{
-                    marginTop: 15,
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}
-                >
-                  <Button disabled>Post Shout</Button>
-                </div>
-              </>
-            )}
-            {!profile && (
-              <LabelMedium marginTop={"20px"}>
-                Want to share your thoughts? Sign in to leave a shout.
-              </LabelMedium>
-            )}
-          </div>
-        </div>
-      )}
+                    }}
+                    maxLength={1000}
+                  />
+                  <div
+                    style={{
+                      marginTop: 15,
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <Button disabled>Post Shout</Button>
+                  </div>
+                </>
+              )}
+              {!profile && (
+                <LabelMedium marginTop={"20px"}>
+                  Want to share your thoughts? Sign in to leave a shout.
+                </LabelMedium>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </Main>
   );
 };
