21b2cf334ba9422ccd3298a93e76c0f66eb3ce51
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-03-23 22:55:52 +0300

    add more logs

--- crates/googledrive/src/repo/google_drive_path.rs
+++ crates/googledrive/src/repo/google_drive_path.rs
@@ -20,7 +20,6 @@ pub async fn create_google_drive_path(
     .execute(pool)
     .await?;
 
-  println!(" {} \n {} \n {} \n {}", google_drive_id, file.id, track.xata_id, file.name);
   println!("{:?}", result);
 
   Ok(())
