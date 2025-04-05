00f17fb8a309e8d7788146b9ffef34546c68695b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-05 11:53:37 +0300

    log album tracks

--- rockskyapi/rocksky-auth/src/users/utils.ts
+++ rockskyapi/rocksky-auth/src/users/utils.ts
@@ -3,10 +3,12 @@ export function dedupeTracksKeepLyrics(tracks) {
 
   for (const track of tracks) {
     const key = track.track_number;
+    console.log("key", key, track);
 
     if (!key) continue;
 
     const existing = trackMap.get(key);
+    console.log("existing", existing);
 
     // If current has lyrics and either no existing or existing has no lyrics, replace it
     if (!existing || (!existing.lyrics && track.lyrics)) {
