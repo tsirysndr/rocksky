adca75a3aa2b2558fc1657376b3b6c628ba4daca
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-07 22:40:10 +0300

    update NowPlayings Modal

--- rockskyweb/src/pages/home/nowplayings/NowPlayings.tsx
+++ rockskyweb/src/pages/home/nowplayings/NowPlayings.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { ChevronLeft, ChevronRight } from "@styled-icons/evaicons-solid";
 import { Modal, ModalBody, ModalHeader, ROLE } from "baseui/modal";
 import { ProgressBar } from "baseui/progress-bar";
 import { StatefulTooltip } from "baseui/tooltip";
@@ -106,6 +107,31 @@ function NowPlayings() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [progress, setProgress] = useState(0);
 
+  const onNext = () => {
+    const nextIndex = currentIndex + 1;
+    if (nextIndex >= nowPlayings.length) {
+      setIsOpen(false);
+      return;
+    }
+
+    setCurrentIndex(nextIndex);
+    setCurrentlyPlaying(nowPlayings[nextIndex]);
+    setProgress(0);
+  };
+
+  const onPrev = () => {
+    const prevIndex = currentIndex - 1;
+
+    if (prevIndex < 0) {
+      setIsOpen(false);
+      return;
+    }
+
+    setCurrentIndex(prevIndex);
+    setCurrentlyPlaying(nowPlayings[prevIndex]);
+    setProgress(0);
+  };
+
   useEffect(() => {
     if (!isOpen) {
       setProgress(0);
@@ -208,19 +234,58 @@ function NowPlayings() {
             marginBottom: 100,
           }}
         >
-          {currentlyPlaying?.track_uri && (
-            <Link to={`/${currentlyPlaying?.track_uri.split("at://")[1]}`}>
-              <Cover src={currentlyPlaying?.album_art} />
-            </Link>
-          )}
-          {currentlyPlaying?.track_uri && (
-            <Link to={`/${currentlyPlaying?.track_uri.split("at://")[1]}`}>
-              <TrackTitle>{currentlyPlaying?.title}</TrackTitle>
-            </Link>
-          )}
-          {!currentlyPlaying?.track_uri && (
-            <Cover src={currentlyPlaying?.album_art} />
-          )}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flex: 1,
+              width: "60vw",
+            }}
+          >
+            <div style={{ display: "flex", alignItems: "center", height: 500 }}>
+              {currentIndex > 0 && (
+                <div style={{ cursor: "pointer" }} onClick={onPrev}>
+                  <ChevronLeft size={50} color="rgba(255, 255, 255, 0.5)" />
+                </div>
+              )}
+            </div>
+            <div
+              style={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              {currentlyPlaying?.track_uri && (
+                <Link to={`/${currentlyPlaying?.track_uri.split("at://")[1]}`}>
+                  <Cover
+                    src={currentlyPlaying?.album_art}
+                    key={currentlyPlaying?.id}
+                  />
+                </Link>
+              )}
+              {currentlyPlaying?.track_uri && (
+                <Link to={`/${currentlyPlaying?.track_uri.split("at://")[1]}`}>
+                  <TrackTitle>{currentlyPlaying?.title}</TrackTitle>
+                </Link>
+              )}
+              {!currentlyPlaying?.track_uri && (
+                <Cover
+                  src={currentlyPlaying?.album_art}
+                  key={currentlyPlaying?.id}
+                />
+              )}
+            </div>
+            <div style={{ display: "flex", alignItems: "center", height: 500 }}>
+              {currentIndex < nowPlayings.length - 1 && (
+                <div style={{ cursor: "pointer" }} onClick={onNext}>
+                  <ChevronRight size={50} color="rgba(255, 255, 255, 0.5)" />
+                </div>
+              )}
+            </div>
+          </div>
+
           {!currentlyPlaying?.track_uri && (
             <TrackTitle>{currentlyPlaying?.title}</TrackTitle>
           )}
