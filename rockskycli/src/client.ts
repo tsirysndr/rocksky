7706c8c34c67073003b0894eb190562593f51afa
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-15 01:20:06 +0300

    [cli] add nowplaying command

--- rockskycli/src/client.ts
+++ rockskycli/src/client.ts
@@ -23,4 +23,26 @@ export class RockskyClient {
 
     return response.json();
   }
+
+  async getNowPlaying(did?: string) {
+    const response = await fetch(
+      `${ROCKSKY_API_URL}/spotify/currently-playing` +
+        (did ? `?did=${did}` : ""),
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch now playing data: ${response.statusText}`
+      );
+    }
+
+    return response.json();
+  }
 }
--- rockskycli/src/index.ts
+++ rockskycli/src/index.ts
@@ -1,3 +1,4 @@
+import { nowplaying } from "cmd/nowplaying";
 import { whoami } from "cmd/whoami";
 import { Command } from "commander";
 import version from "../package.json" assert { type: "json" };
@@ -22,5 +23,14 @@ program
   .command("whoami")
   .description("Get the current logged-in user.")
   .action(whoami);
+
+program
+  .command("nowplaying")
+  .argument(
+    "[did]",
+    "The DID or handle of the user to get the now playing track for."
+  )
+  .description("Get the currently playing track.")
+  .action(nowplaying);
 
 program.parse(process.argv);
