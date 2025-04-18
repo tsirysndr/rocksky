05556de52621f111e160ccfded4bd8ccff9db253
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-18 07:58:44 +0300

    [api] update opentelemetry config

--- rockskyapi/rocksky-auth/src/tracing.ts
+++ rockskyapi/rocksky-auth/src/tracing.ts
@@ -1,6 +1,4 @@
-const {
-  getNodeAutoInstrumentations,
-} = require("@opentelemetry/auto-instrumentations-node");
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 
