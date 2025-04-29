f07c863ce5a6f6cb95d6a460def7414226395ef0
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-29 10:49:15 +0300

    [spotify] fix nowplaying issue

--- crates/spotify/src/main.rs
+++ crates/spotify/src/main.rs
@@ -436,11 +436,13 @@ pub async fn watch_currently_playing(spotify_email: String, token: String, did: 
         }
         let mut current_song = serde_json::from_str::<CurrentlyPlaying>(&cached)?;
 
-        if current_song.is_playing && current_song.progress_ms.unwrap_or(0) < current_song.item.clone().unwrap().duration_ms.into() {
-          current_song.progress_ms = Some(current_song.progress_ms.unwrap_or(0) + 500);
-          cache_clone.setex(&format!("{}:current", spotify_email_clone), &serde_json::to_string(&current_song)?, 16)?;
-          thread::sleep(std::time::Duration::from_millis(500));
-          continue;
+        if let Some(item) = current_song.item.clone() {
+          if current_song.is_playing && current_song.progress_ms.unwrap_or(0) < item.duration_ms.into() {
+            current_song.progress_ms = Some(current_song.progress_ms.unwrap_or(0) + 500);
+            cache_clone.setex(&format!("{}:current", spotify_email_clone), &serde_json::to_string(&current_song)?, 16)?;
+            thread::sleep(std::time::Duration::from_millis(500));
+            continue;
+          }
         }
         continue;
       }
