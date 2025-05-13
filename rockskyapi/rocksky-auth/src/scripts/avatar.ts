7a784119e7405e2222dcd48e96415201e62753a1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-05-13 09:22:17 +0300

    add script for updating user avatar

---
+++ rockskyapi/rocksky-auth/src/scripts/avatar.ts
@@ -0,0 +1,48 @@
+import { equals } from "@xata.io/client";
+import { ctx } from "context";
+import { eq } from "drizzle-orm";
+import _ from "lodash";
+import users from "schema/users";
+
+const args = process.argv.slice(2);
+
+for (const did of args) {
+  const user = await ctx.client.db.users
+    .filter({
+      $any: [{ did }, { handle: did }],
+    })
+    .getFirst();
+  if (!user) {
+    console.log(`User ${did} not found`);
+    continue;
+  }
+
+  const plc = await fetch(`https://plc.directory/${user.did}`).then((res) =>
+    res.json()
+  );
+
+  const serviceEndpoint = _.get(plc, "service.0.serviceEndpoint");
+  if (!serviceEndpoint) {
+    console.log(`Service endpoint not found for ${did}`);
+    continue;
+  }
+
+  const profile = await fetch(
+    `${serviceEndpoint}/xrpc/com.atproto.repo.getRecord?repo=${did}&collection=app.bsky.actor.profile&rkey=self`
+  ).then((res) => res.json());
+  const ref = _.get(profile, "value.avatar.ref.$link");
+  const type = _.get(profile, "value.avatar.mimeType").split("/")[1];
+  await ctx.db
+    .update(users)
+    .set({
+      avatar: `https://cdn.bsky.app/img/avatar/plain/${did}/${ref}@${type}`,
+    })
+    .where(eq(users.did, did))
+    .execute();
+
+  const u = await ctx.client.db.users
+    .select(["*"])
+    .filter("did", equals(did))
+    .getFirst();
+  ctx.nc.publish("rocksky.user", Buffer.from(JSON.stringify(u)));
+}
