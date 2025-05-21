47cf3f54e5039c742e6c64f275510ef3e2403ada
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-05-22 01:18:28 +0300

    [web] add webscrcobbler setup instructions

--- rockskyweb/src/layouts/Navbar/Navbar.tsx
+++ rockskyweb/src/layouts/Navbar/Navbar.tsx
@@ -1,13 +1,19 @@
 import styled from "@emotion/styled";
+import { Copy } from "@styled-icons/ionicons-outline";
+import { useQuery } from "@tanstack/react-query";
 import { Avatar } from "baseui/avatar";
 import { NestedMenus, StatefulMenu } from "baseui/menu";
+import { Modal, ModalBody, ModalHeader } from "baseui/modal";
 import { PLACEMENT, StatefulPopover } from "baseui/popover";
 import { DURATION, useSnackbar } from "baseui/snackbar";
+import { StatefulTooltip } from "baseui/tooltip";
 import { LabelMedium } from "baseui/typography";
+import copy from "copy-to-clipboard";
 import { useAtomValue, useSetAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { profileAtom } from "../../atoms/profile";
+import { API_URL } from "../../consts";
 
 const Container = styled.div`
   position: fixed;
@@ -26,12 +32,41 @@ const Container = styled.div`
   }
 `;
 
+export const Code = styled.div`
+  background-color: #000;
+  color: #fff;
+  padding: 5px;
+  display: inline-block;
+  border-radius: 5px;
+`;
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   const setProfile = useSetAtom(profileAtom);
   const profile = useAtomValue(profileAtom);
   const navigate = useNavigate();
   const jwt = localStorage.getItem("token");
   const { enqueue } = useSnackbar();
+
+  const { data } = useQuery({
+    queryKey: ["webscrobbler"],
+    queryFn: async () => {
+      const response = await fetch(`${API_URL}/webscrobbler`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      return response.json();
+    },
+  });
+
+  const webscrobblerWebhook = useMemo(() => {
+    if (data) {
+      return `https://webscrobbler.rocksky.app/${data.webscrobblers.uuid}`;
+    }
+    return "";
+  }, [data]);
 
   useEffect(() => {
     if (profile?.spotifyConnected && !!localStorage.getItem("spotify")) {
@@ -44,6 +79,10 @@ function Navbar() {
       );
     }
   }, [enqueue, profile]);
+
+  const close = () => {
+    setIsOpen(false);
+  };
 
   return (
     <Container>
@@ -77,6 +116,10 @@ function Navbar() {
                     label: <LabelMedium>API Applications</LabelMedium>,
                   },
                   {
+                    id: "webscrobbler",
+                    label: <LabelMedium>Web Scrobbler</LabelMedium>,
+                  },
+                  {
                     id: "signout",
                     label: <LabelMedium>Sign out</LabelMedium>,
                   },
@@ -94,6 +137,9 @@ function Navbar() {
                       localStorage.removeItem("token");
                       localStorage.removeItem("did");
                       window.location.href = "/";
+                      break;
+                    case "webscrobbler":
+                      setIsOpen(true);
                       break;
                     default:
                       break;
@@ -122,6 +168,54 @@ function Navbar() {
           </button>
         </StatefulPopover>
       )}
+
+      <Modal
+        onClose={close}
+        isOpen={isOpen}
+        overrides={{
+          Root: {
+            style: {
+              zIndex: 1,
+            },
+          },
+        }}
+        size={650}
+      >
+        <ModalHeader>Setup Web Scrobbler</ModalHeader>
+        <ModalBody>
+          <LabelMedium>
+            To use the Web Scrobbler, you need to install the browser extension
+            and connect it to Rocksky.
+          </LabelMedium>
+          <div className="mt-[20px]">
+            <a
+              href="https://web-scrobbler.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: "rgb(255, 40, 118)",
+              }}
+            >
+              Install Web Scrobbler
+            </a>
+          </div>
+          <div style={{ marginTop: 20 }}>
+            <LabelMedium>
+              After installing the extension, add the following URL to the
+              extension settings as a custom API URL:
+            </LabelMedium>
+            <Code className="mt-[15px]">{webscrobblerWebhook}</Code>
+            <StatefulTooltip content="Copy API Key">
+              <Copy
+                onClick={() => copy(webscrobblerWebhook)}
+                size={18}
+                color="#000000a0"
+                style={{ marginLeft: 5, cursor: "pointer" }}
+              />
+            </StatefulTooltip>
+          </div>
+        </ModalBody>
+      </Modal>
     </Container>
   );
 }
