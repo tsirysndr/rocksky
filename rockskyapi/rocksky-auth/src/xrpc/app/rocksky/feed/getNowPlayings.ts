a8d771d7892aac0c2195840a39b1fd4af923e7b8
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-06-09 13:38:33 +0300

    implement getNowPlayings xrpc method

--- rockskyapi/rocksky-auth/src/xrpc/app/rocksky/feed/getNowPlayings.ts
+++ rockskyapi/rocksky-auth/src/xrpc/app/rocksky/feed/getNowPlayings.ts
@@ -1,12 +1,26 @@
 import { Context } from "context";
-import { pipe } from "effect";
+import { Effect, pipe } from "effect";
 import { Server } from "lexicon";
+import { NowPlayingView } from "lexicon/types/app/rocksky/feed/defs";
+import { QueryParams } from "lexicon/types/app/rocksky/feed/getNowPlayings";
+import { deepCamelCaseKeys } from "lib";
 
 export default function (server: Server, ctx: Context) {
-  const getNowPlayings = (params) => pipe(params, retrieve, presentation);
+  const getNowPlayings = (params) =>
+    pipe(
+      { params, ctx },
+      retrieve,
+      Effect.flatMap(presentation),
+      Effect.retry({ times: 3 }),
+      Effect.timeout("10 seconds"),
+      Effect.catchAll((err) => {
+        console.error(err);
+        return Effect.succeed({});
+      })
+    );
   server.app.rocksky.feed.getNowPlayings({
     handler: async ({ params }) => {
-      const result = getNowPlayings(params);
+      const result = await Effect.runPromise(getNowPlayings(params));
       return {
         encoding: "application/json",
         body: result,
@@ -15,14 +29,24 @@ export default function (server: Server, ctx: Context) {
   });
 }
 
-const retrieve = () => {
-  // Logic to retrieve now playing songs
-  return [];
+const retrieve = ({ params, ctx }: { params: QueryParams; ctx: Context }) => {
+  return Effect.tryPromise({
+    try: () =>
+      ctx.analytics.post("library.getDistinctScrobbles", {
+        pagination: {
+          skip: 0,
+          take: params.size,
+        },
+      }),
+    catch: (error) =>
+      new Error(`Failed to retrieve now playing songs: ${error}`),
+  });
 };
 
-const presentation = (nowPlayings) => {
-  // Logic to format the now playing songs for presentation
-  return {
-    nowPlayings: [],
-  };
+const presentation = (
+  data
+): Effect.Effect<{ nowPlayings: NowPlayingView[] }, never> => {
+  return Effect.sync(() => ({
+    nowPlayings: deepCamelCaseKeys(data),
+  }));
 };
