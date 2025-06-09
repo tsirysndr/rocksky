9954127a22d6f8179bdbee6e504f7adcfb8b17f2
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-06-09 11:22:44 +0300

    implement getTracks xrpc method

--- rockskyapi/rocksky-auth/src/xrpc/app/rocksky/song/getSongs.ts
+++ rockskyapi/rocksky-auth/src/xrpc/app/rocksky/song/getSongs.ts
@@ -1,12 +1,26 @@
 import { Context } from "context";
-import { pipe } from "effect";
+import { Effect, pipe } from "effect";
 import { Server } from "lexicon";
+import { SongViewBasic } from "lexicon/types/app/rocksky/song/defs";
+import { QueryParams } from "lexicon/types/app/rocksky/song/getSongs";
+import { deepCamelCaseKeys } from "lib";
 
 export default function (server: Server, ctx: Context) {
-  const getSongs = (params) => pipe(params, retrieve, presentation);
+  const getSongs = (params) =>
+    pipe(
+      { params, ctx },
+      retrieve,
+      Effect.flatMap(presentation),
+      Effect.retry({ times: 3 }),
+      Effect.timeout("10 seconds"),
+      Effect.catchAll((err) => {
+        console.error(err);
+        return Effect.succeed({ albums: [] });
+      })
+    );
   server.app.rocksky.song.getSongs({
     handler: async ({ params }) => {
-      const result = getSongs(params);
+      const result = await Effect.runPromise(getSongs(params));
       return {
         encoding: "application/json",
         body: result,
@@ -15,12 +29,49 @@ export default function (server: Server, ctx: Context) {
   });
 }
 
-const retrieve = (params) => {
-  // Logic to retrieve songs based on params
-  return [];
+const retrieve = ({
+  params,
+  ctx,
+}: {
+  params: QueryParams;
+  ctx: Context;
+}): Effect.Effect<{ data: Track[] }, Error> => {
+  return Effect.tryPromise({
+    try: () =>
+      ctx.analytics.post("library.getTracks", {
+        pagination: {
+          skip: params.offset || 0,
+          take: params.limit || 100,
+        },
+      }),
+    catch: (error) => new Error(`Failed to retrieve tracks: ${error}`),
+  });
 };
 
-const presentation = (songs) => {
-  // Logic to format the songs for presentation
-  return {};
+const presentation = ({
+  data,
+}: {
+  data: Track[];
+}): Effect.Effect<{ tracks: SongViewBasic[] }, never> => {
+  return Effect.sync(() => ({ tracks: deepCamelCaseKeys(data) }));
+};
+
+type Track = {
+  id: string;
+  uri: string;
+  unique_listeners: number;
+  play_count: number;
+  title: string;
+  artist: string;
+  artist_uri: string;
+  album: string;
+  album_uri: string;
+  album_art: string;
+  album_artist: string;
+  copyright_message: string;
+  disc_number: number;
+  duration: number;
+  sha256: string;
+  track_number: number;
+  created_at: string;
 };
