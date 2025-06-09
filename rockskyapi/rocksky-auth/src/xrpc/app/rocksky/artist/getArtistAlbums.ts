818e8c777640e58996565efac9918d272a234400
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-06-09 09:15:29 +0300

    implement getArtistAlbums xrpc method

--- rockskyapi/rocksky-auth/src/xrpc/app/rocksky/artist/getArtistAlbums.ts
+++ rockskyapi/rocksky-auth/src/xrpc/app/rocksky/artist/getArtistAlbums.ts
@@ -1,12 +1,26 @@
 import { Context } from "context";
-import { pipe } from "effect";
+import { Effect, pipe } from "effect";
 import { Server } from "lexicon";
+import { AlbumViewBasic } from "lexicon/types/app/rocksky/album/defs";
+import { QueryParams } from "lexicon/types/app/rocksky/artist/getArtistAlbums";
+import { deepCamelCaseKeys } from "lib";
 
 export default function (server: Server, ctx: Context) {
-  const getArtistAlbums = (params) => pipe(params, retrieve, presentation);
+  const getArtistAlbums = (params) =>
+    pipe(
+      { params, ctx },
+      retrieve,
+      Effect.flatMap(presentation),
+      Effect.retry({ times: 3 }),
+      Effect.timeout("10 seconds"),
+      Effect.catchAll((err) => {
+        console.error(err);
+        return Effect.succeed({ albums: [] });
+      })
+    );
   server.app.rocksky.artist.getArtistAlbums({
     handler: async ({ params }) => {
-      const result = getArtistAlbums(params);
+      const result = await Effect.runPromise(getArtistAlbums(params));
       return {
         encoding: "application/json",
         body: result,
@@ -15,14 +29,42 @@ export default function (server: Server, ctx: Context) {
   });
 }
 
-const retrieve = () => {
-  // Logic to retrieve albums for the artist
-  return [];
+const retrieve = ({
+  params,
+  ctx,
+}: {
+  params: QueryParams;
+  ctx: Context;
+}): Effect.Effect<{ data: Album[] }, Error> => {
+  return Effect.tryPromise({
+    try: () =>
+      ctx.analytics.post("library.getArtistAlbums", {
+        pagination: {
+          artist_id: params.uri,
+        },
+      }),
+    catch: (error) => new Error(`Failed to retrieve artist's albums: ${error}`),
+  });
 };
 
-const presentation = () => {
-  // Logic to format the albums for presentation
-  return {
-    albums: [],
-  };
+const presentation = ({
+  data,
+}: {
+  data: Album[];
+}): Effect.Effect<{ albums: AlbumViewBasic[] }, never> => {
+  return Effect.sync(() => ({ albums: deepCamelCaseKeys(data) }));
+};
+
+type Album = {
+  id: string;
+  uri: string;
+  title: string;
+  artist: string;
+  artist_uri: string;
+  year: number;
+  album_art: string;
+  release_date: string;
+  sha256: string;
+  play_count: number;
+  unique_listeners: number;
 };
