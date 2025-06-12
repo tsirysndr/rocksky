14b1430d3f387e19de77c66b8938f118dcbc932d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-06-12 22:55:42 +0300

    return uri and liked in getCurrentlyPlaying (spotify) response

--- rockskyapi/rocksky-auth/src/xrpc/app/rocksky/spotify/getCurrentlyPlaying.ts
+++ rockskyapi/rocksky-auth/src/xrpc/app/rocksky/spotify/getCurrentlyPlaying.ts
@@ -1,9 +1,10 @@
 import { HandlerAuth } from "@atproto/xrpc-server";
 import { Context } from "context";
-import { eq, or } from "drizzle-orm";
-import { Effect, pipe } from "effect";
+import { and, eq, or } from "drizzle-orm";
+import { Effect, Match, pipe } from "effect";
 import { Server } from "lexicon";
 import { QueryParams } from "lexicon/types/app/rocksky/spotify/getCurrentlyPlaying";
+import { createHash } from "node:crypto";
 import tables from "schema";
 import { SelectSpotifyAccount } from "schema/spotify-accounts";
 import { SelectUser } from "schema/users";
@@ -15,6 +16,7 @@ export default function (server: Server, ctx: Context) {
       withUser,
       Effect.flatMap(withSpotifyAccount),
       Effect.flatMap(retrieve),
+      Effect.flatMap(withUriAndLikes),
       Effect.flatMap(presentation),
       Effect.retry({ times: 3 }),
       Effect.timeout("10 seconds"),
@@ -111,26 +113,73 @@ const withSpotifyAccount = ({
 const retrieve = ({
   spotifyAccount,
   ctx,
+  user,
 }: {
   spotifyAccount: SelectSpotifyAccount;
   user: SelectUser;
   ctx: Context;
   params: QueryParams;
   did?: string;
-}): Effect.Effect<{}, Error> => {
+}): Effect.Effect<any, Error> => {
   return Effect.tryPromise({
     try: async () =>
-      ctx.redis.get(`${spotifyAccount.email}:current`).then((cached) => {
-        if (cached) {
-          return JSON.parse(cached);
-        }
-        return {};
-      }),
+      ctx.redis
+        .get(`${spotifyAccount.email}:current`)
+        .then((cached) =>
+          Match.value(cached).pipe(
+            Match.when(null, () => ({})),
+            Match.when(undefined, () => ({})),
+            Match.orElse(() => JSON.parse(cached))
+          )
+        )
+        .then((cached) => [cached, ctx, user]),
     catch: (error) =>
       new Error(`Failed to retrieve currently playing: ${error}`),
   });
 };
 
-const presentation = (currentlyPlaying): Effect.Effect<any, never> => {
-  return Effect.sync(() => currentlyPlaying);
+const withUriAndLikes = ([track, ctx, user]: [any, Context, SelectUser]) => {
+  return Effect.tryPromise({
+    try: async () => {
+      const sha256 = createHash("sha256")
+        .update(
+          `${track.item.name} - ${track.item.artists.map((x) => x.name).join(", ")} - ${track.item.album.name}`.toLowerCase()
+        )
+        .digest("hex");
+      const [record] = await ctx.db
+        .select()
+        .from(tables.tracks)
+        .where(eq(tables.tracks.sha256, sha256))
+        .execute();
+      return ctx.db
+        .select()
+        .from(tables.lovedTracks)
+        .leftJoin(
+          tables.tracks,
+          eq(tables.lovedTracks.trackId, tables.tracks.id)
+        )
+        .leftJoin(tables.users, eq(tables.lovedTracks.userId, tables.users.id))
+        .where(
+          and(eq(tables.tracks.sha256, sha256), eq(tables.users.did, user.did))
+        )
+        .execute()
+        .then((results) =>
+          Match.value(track).pipe(
+            Match.when({}, () => ({})),
+            Match.orElse(() => ({
+              ...track,
+              songUri: record?.uri,
+              artistUri: record?.artistUri,
+              albumUri: record?.albumUri,
+              liked: results.length > 0,
+            }))
+          )
+        );
+    },
+    catch: (error) => new Error(`Failed to retrieve URI and likes: ${error}`),
+  });
+};
+
+const presentation = (track): Effect.Effect<any, never> => {
+  return Effect.sync(() => track);
 };
