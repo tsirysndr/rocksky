679de984000dc260980a34933d308bb7560ac471
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-09-07 22:40:04 +0300

    setup nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+         buildInputs = [
+          pkgs.cargo
+          pkgs.rustc
+          pkgs.rustfmt
+          pkgs.rustPackages.clippy
+          pkgs.bun
+          pkgs.nodejs
+          pkgs.duckdb
+         ];
+       };
+    });
+}
