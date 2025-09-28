1a1d049d84f2dcf07ea2d5e98a1f04747e3a2771
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-09-28 04:54:11 +0300

    fix: update feed and lib modules to return Result types for better error handling

--- crates/feed/src/feed.rs
+++ crates/feed/src/feed.rs
@@ -1,8 +1,13 @@
 use crate::config::Config;
 use crate::types::{DidDocument, FeedSkeletonParameters, Service};
 use crate::{feed_handler::FeedHandler, types::FeedSkeletonQuery};
+use anyhow::Error;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+use std::env;
 use std::fmt::Debug;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use warp::Filter;
 
 /// A `Feed` stores a `FeedHandler`, handles feed server endpoints & connects to the Firehose using the `start` methods.
@@ -21,7 +26,7 @@ pub trait Feed<Handler: FeedHandler + Clone + Send + Sync + 'static> {
         &mut self,
         name: impl AsRef<str>,
         address: impl Into<SocketAddr> + Debug + Clone + Send,
-    ) -> impl std::future::Future<Output = ()> + Send {
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send {
         self.start_with_config(name, Config::load_env_config(), address)
     }
 
@@ -39,13 +44,19 @@ pub trait Feed<Handler: FeedHandler + Clone + Send + Sync + 'static> {
         name: impl AsRef<str>,
         config: Config,
         address: impl Into<SocketAddr> + Debug + Clone + Send,
-    ) -> impl std::future::Future<Output = ()> + Send {
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send {
         let handler = self.handler();
         let address = address.clone();
         let feed_name = name.as_ref().to_string();
 
         async move {
             let config = config;
+            let pool = PgPoolOptions::new()
+                .max_connections(5)
+                .connect(&env::var("XATA_POSTGRES_URL")?)
+                .await?;
+            let pool = Arc::new(pool);
+            let db_filter = warp::any().map(move || pool.clone());
 
             let did_config = config.clone();
             let did_json = warp::path(".well-known")
@@ -56,16 +67,22 @@ pub trait Feed<Handler: FeedHandler + Clone + Send + Sync + 'static> {
             let describe_feed_generator = warp::path("xrpc")
                 .and(warp::path("app.rocksky.feed.describeFeedGenerator"))
                 .and(warp::get())
-                .and_then(move || describe_feed_generator(feed_name.clone()));
+                .and(db_filter.clone())
+                .and_then(move |_pool: Arc<Pool<Postgres>>| {
+                    describe_feed_generator(feed_name.clone())
+                });
 
             let get_feed_handler = handler.clone();
             let get_feed_skeleton = warp::path("xrpc")
                 .and(warp::path("app.rocksky.feed.getFeedSkeleton"))
                 .and(warp::get())
                 .and(warp::query::<FeedSkeletonParameters>())
-                .and_then(move |query: FeedSkeletonParameters| {
-                    get_feed_skeleton::<Handler>(query.into(), get_feed_handler.clone())
-                });
+                .and(db_filter.clone())
+                .and_then(
+                    move |query: FeedSkeletonParameters, _pool: Arc<Pool<Postgres>>| {
+                        get_feed_skeleton::<Handler>(query.into(), get_feed_handler.clone())
+                    },
+                );
 
             let api = did_json.or(describe_feed_generator).or(get_feed_skeleton);
 
@@ -101,6 +118,8 @@ pub trait Feed<Handler: FeedHandler + Clone + Send + Sync + 'static> {
             tokio::join!(feed_server.run(address), firehose_listener)
                 .1
                 .expect("Couldn't await tasks");
+
+            Ok::<(), Error>(())
         }
     }
 }
