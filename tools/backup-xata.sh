893944838f6ee3103c6fa3d88dc62b1b17db01c7
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-10-04 19:25:04 +0300

    fix: change backup file extension from .sql.gz to .dump for consistency

--- tools/backup-xata.sh
+++ tools/backup-xata.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 # exit if XATA_POSTGRES_URL is not set
 : "${XATA_POSTGRES_URL:?Need to set XATA_POSTGRES_URL non-empty}"
 
-BACKUP_NAME="xata-backup-$(date +%Y%m%d-%H%M%S).sql.gz"
+BACKUP_NAME="xata-backup-$(date +%Y%m%d-%H%M%S).dump"
 
-pg_dump $XATA_POSTGRES_URL --no-owner --no-acl -Fc | pv -pteba | gzip > "$BACKUP_NAME"
+pg_dump $XATA_POSTGRES_URL --no-owner --no-acl -Fc | pv -pteba > "$BACKUP_NAME"
