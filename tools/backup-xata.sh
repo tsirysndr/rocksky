dca2ac88b7d68e4fd1965a46e144aeb02776117f
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-10-04 23:01:24 +0300

    fix: remove obsolete backup script for Xata

--- tools/backup-xata.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# exit if XATA_POSTGRES_URL is not set
-: "${XATA_POSTGRES_URL:?Need to set XATA_POSTGRES_URL non-empty}"
-
-BACKUP_NAME="xata-backup-$(date +%Y%m%d-%H%M%S).dump"
-
-pg_dump $XATA_POSTGRES_URL --no-owner --no-acl -Fc | pv -pteba > "$BACKUP_NAME"
