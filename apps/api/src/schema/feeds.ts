299fbe6502a8fcd552ae87ecbab036d690ccafc5
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-10-11 11:02:19 +0300

    feat: add feeds schema and related types; update package scripts
    - Added a new schema for feeds in `src/schema/feeds.ts` with fields for id, displayName, description, did, uri, userId, xataVersion, createdAt, and updatedAt.
    - Introduced types for selecting and inserting feeds using Drizzle ORM.
    - Updated `package.json` to include a new script for feeding data: `feed`.
    - Modified `_journal.json` to include a new entry for "whole greymalkin".

---
+++ apps/api/src/schema/feeds.ts
@@ -0,0 +1,25 @@
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { sql } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import users from "./users";
+
+const feeds = pgTable("feeds", {
+  id: text("xata_id")
+    .primaryKey()
+    .default(sql`xata_id()`),
+  displayName: text("display_name").notNull(),
+  description: text("description"),
+  did: text("did").notNull().unique(),
+  uri: text("uri").notNull().unique(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  xataVersion: integer("xata_version"),
+  createdAt: timestamp("xata_createdat").defaultNow().notNull(),
+  updatedAt: timestamp("xata_updatedat").defaultNow().notNull(),
+});
+
+export type SelectFeed = InferSelectModel<typeof feeds>;
+export type InsertFeed = InferInsertModel<typeof feeds>;
+
+export default feeds;
