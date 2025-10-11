df112b15067c9b3bef69333ad16ff9fa33b0d0d4
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-10-11 21:25:29 +0300

    work in progress

--- apps/api/src/scripts/feed.ts
+++ apps/api/src/scripts/feed.ts
@@ -1,4 +1,15 @@
+import chalk from "chalk";
+import { ctx } from "context";
+import { createAgent } from "lib/agent";
 import prompts from "prompts";
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Please provide user author identifier (handle or DID).");
+  console.log(`Usage: ${chalk.cyan("npm run feed -- <handle|did>")}`);
+  process.exit(1);
+}
 
 const name = await prompts({
   type: "text",
@@ -30,8 +41,25 @@ const did = await prompts({
   message: "What is the feed DID?",
 });
 
+if (!/^did:web:[a-zA-Z0-9_.-]{3,30}$/.test(did.value)) {
+  console.error(
+    "Invalid DID format. It should start with 'did:web:' followed by 3 to 30 alphanumeric characters, underscores, hyphens, or periods."
+  );
+  process.exit(1);
+}
+
 console.log("Feed name:", name.value);
 console.log("Description:", description.value);
 console.log("DID:", did.value);
+
+let userDid = args[0];
+
+if (!userDid.startsWith("did:plc:")) {
+  userDid = await ctx.baseIdResolver.handle.resolve(userDid);
+}
+
+const agent = await createAgent(ctx.oauthClient, userDid);
+
+console.log("Creating feed...");
 
 process.exit(0);
