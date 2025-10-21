2dfe9526f8f36420edf24a8f36bf2cf0b1e372f0
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-10-21 07:38:34 +0300

    fix: ensure avatar update is skipped based on environment variable

--- apps/api/src/scripts/avatar.ts
+++ apps/api/src/scripts/avatar.ts
@@ -17,29 +17,33 @@ for (const did of args) {
     continue;
   }
 
-  const plc = await fetch(`https://plc.directory/${user.did}`).then((res) =>
-    res.json()
-  );
+  if (!process.env.SKIP_AVATAR_UPDATE) {
+    const plc = await fetch(`https://plc.directory/${user.did}`).then((res) =>
+      res.json()
+    );
 
-  const serviceEndpoint = _.get(plc, "service.0.serviceEndpoint");
-  if (!serviceEndpoint) {
-    console.log(`Service endpoint not found for ${did}`);
-    continue;
+    const serviceEndpoint = _.get(plc, "service.0.serviceEndpoint");
+    if (!serviceEndpoint) {
+      console.log(`Service endpoint not found for ${did}`);
+      continue;
+    }
+
+    const profile = await fetch(
+      `${serviceEndpoint}/xrpc/com.atproto.repo.getRecord?repo=${user.did}&collection=app.bsky.actor.profile&rkey=self`
+    ).then((res) => res.json());
+    const ref = _.get(profile, "value.avatar.ref.$link");
+    const type = _.get(profile, "value.avatar.mimeType", "").split("/")[1];
+    await ctx.db
+      .update(users)
+      .set({
+        displayName: _.get(profile, "value.displayName"),
+        avatar: `https://cdn.bsky.app/img/avatar/plain/${user.did}/${ref}@${type}`,
+      })
+      .where(eq(users.did, user.did))
+      .execute();
+  } else {
+    console.log(`Skipping avatar update for ${did}`);
   }
-
-  const profile = await fetch(
-    `${serviceEndpoint}/xrpc/com.atproto.repo.getRecord?repo=${user.did}&collection=app.bsky.actor.profile&rkey=self`
-  ).then((res) => res.json());
-  const ref = _.get(profile, "value.avatar.ref.$link");
-  const type = _.get(profile, "value.avatar.mimeType", "").split("/")[1];
-  await ctx.db
-    .update(users)
-    .set({
-      displayName: _.get(profile, "value.displayName"),
-      avatar: `https://cdn.bsky.app/img/avatar/plain/${user.did}/${ref}@${type}`,
-    })
-    .where(eq(users.did, user.did))
-    .execute();
 
   const [u] = await ctx.db
     .select()
