384208462702de7a33e4032dfb46b61f0b4f9ed7
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-02-11 12:34:24 +0300

    Set Postgres pool idle timeout to 10 minutes

--- apps/api/src/drizzle.ts
+++ apps/api/src/drizzle.ts
@@ -2,7 +2,11 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "lib/env";
 import pg from "pg";
 
-const pool = new pg.Pool({ connectionString: env.XATA_POSTGRES_URL, max: 20 });
+const pool = new pg.Pool({
+  connectionString: env.XATA_POSTGRES_URL,
+  max: 20,
+  idleTimeoutMillis: 10 * 1000 * 60,
+});
 const db = drizzle(pool);
 
 export default { db };
