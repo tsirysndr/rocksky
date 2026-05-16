da39e97cf1aeb8c2a11b491a2b74a9c682e1fd6f
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-05-16 19:10:58 +0300

    app: bump version to 1.1.0

--- apps/app/app.config.js
+++ apps/app/app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "Rocksky",
   slug: "rocksky",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "rocksky",
