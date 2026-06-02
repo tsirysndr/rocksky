c2319ca9dd2cd0fd3fb208d40e20f17a02a56ca9
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-02 11:39:18 +0300

    move swift sdk to another repo

--- sdk/swift/Sources/Rocksky/Models/GoogleDrive.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct GoogleDriveFileView: Codable, Hashable, Sendable {
-    public let id: String?
-}
-
-public struct GoogleDriveFileListView: Codable, Sendable {
-    public let files: [GoogleDriveFileView]?
-}
--- sdk/swift/Sources/Rocksky/Models/Player.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct CurrentlyPlayingView: Codable, Hashable, Sendable {
-    public let title: String?
-}
-
-public struct PlaybackQueueView: Codable, Sendable {
-    public let tracks: [SongViewBasic]?
-}
