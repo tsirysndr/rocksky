a95c35df7eeeee384235603031aacd93bcaa4ddb
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-08-22 22:23:02 +0300

    sdk: update manifests

--- sdk/elixir/mix.exs
+++ sdk/elixir/mix.exs
@@ -37,7 +37,7 @@ defmodule Rocksky.MixProject do
 
   defp rocksky_erl_dep do
     case System.get_env("ROCKSKY_ERL_PATH") do
-      nil -> "~> 0.7"
+      nil -> "~> 0.8"
       path -> [path: path]
     end
   end
