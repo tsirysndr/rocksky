67a1971364bb45edaf37220d36e0c7d8286cf847
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-08-23 08:03:27 +0300

    remove rockbox* packages

--- apps/rockbox-router/src/logger.ts
+++
@@ -1,9 +0,0 @@
-import { createConsola } from "consola";
-
-export const logger = createConsola({
-  level: Number(process.env.LOG_LEVEL ?? 4),
-  formatOptions: {
-    date: true,
-    colors: process.stdout.isTTY ?? false,
-  },
-}).withTag("rockbox-router");
